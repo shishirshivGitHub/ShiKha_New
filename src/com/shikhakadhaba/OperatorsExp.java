e8f3722bcea7f76e5caa9732e269dbd5c68dcb38
Shishirshiv007 <shishirshiv007@gmail.com>
2022-01-04 18:30:22 +0800

    operators

--- src/com/shikhakadhaba/OperatorsExp.java
+++ src/com/shikhakadhaba/OperatorsExp.java
@@ -9,7 +9,31 @@ public class OperatorsExp {
         int sum1=100;
         int sum2=sum1+100;
         int sum3=sum1+sum2;
+
+        boolean A = true;
+        boolean B = false;
+
+        int a=10, b=20 ,and, or, comp, xor, rs, Is;
+        and =a&b;
+        or = b|a;
+        comp= ~a;
+        xor = a^b;
+        rs = a<<b;
+        Is = b>>a;
+
         System.out.println(sum3);
+        System.out.println((A&B) +" ");
+           System.out.println((A&B) +" ");
+           System.out.println((A|B) +" ");
+           System.out.println((A^B) +" ");
+           System.out.println((!B) +" ");
+           System.out.println(((A&B)|B) +" ");
+
+           System.out.println("and" +and);
+           System.out.println("Is" +Is);
+
+
+
 
 
 
